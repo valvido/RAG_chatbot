37e0b5dbc925201034416756b59265773c772333
tina <vidovcv33@gmail.com>
2025-10-25 19:21:17 +0200

    setting up the typescript project

---
+++ rag-chatbot/scripts/loadDB.ts
@@ -0,0 +1,6 @@
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+import OpenAI from "openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";   
+
+import "dotenv/config";
